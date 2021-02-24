588369ac7deb44a7bdafa16b3d5ea33798cb8501
Ronald <ronald.drp11@gmail.com>
2021-02-24 12:38:26 -0300

    added setup.py to install package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pac-gnn',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Pick and Choose Graph Neural Network'
+)
